3ef0908dcee34d65e4484c323fa9e98946b22b72
Walter Rowe <walter.rowe@gmail.com>
2022-05-19 20:20:42 -0400

    added code to generate date-named output file

--- project-status.py
+++ project-status.py
@@ -3,52 +3,87 @@
 
 from datetime import *
 
-issues = [
-    { "title": "issue1", "labels": [ 'Overdue' ], "closed": date.today() - timedelta(10) },
-    { "title": "issue2", "labels": [ ], "closed": date.today() + timedelta(10) },
-    { "title": "issue3", "labels": [ ], "closed": date.today() + timedelta(3) },
-    { "title": "issue4", "labels": [ ], "closed": date.today() - timedelta(7) }
-]
+class Issue():
+    def __init__(self, title, labels, closed):
+        self.title = title
+        self.labels = labels
+        self.closed = closed
+
+    def __repr__(self):
+        return str(self.__class__) + ': ' + str(self.__dict__)
+
+    def __str__(self):
+        return f'Title: {self.title}, Closed: {self.closed}, Labels: {self.labels}'
+
+    def __add__(x, y):
+        return [ x, y ]
+
+    def print(self):
+        if isinstance(self,list):
+            for mine in self:
+                mine.print
+        else:
+            print (f'Title: {self.title}, Closed: {self.closed}, Labels: {self.labels}')
+
+issues = []
+# issues_list = [
+#     { "title": "issue1", "labels": [ 'Overdue' ], "closed": today - timedelta(10) },
+#     { "title": "issue2", "labels": [ ], "closed": date.today() + timedelta(10) },
+#     { "title": "issue3", "labels": [ ], "closed": date.today() + timedelta(3) },
+#     { "title": "issue4", "labels": [ ], "closed": date.today() - timedelta(7) }
+# ]
 
 today = date.today()
 thirty_last = today - timedelta(30)
 thirty_next = today + timedelta(30)
 
+issues = issues + [ Issue(title="issue1", closed=(today-timedelta(10)), labels=[ 'Overdue' ] ) ]
+issues = issues + [ Issue(title="issue2", closed=(today+timedelta(10)), labels=[] ) ]
+issues = issues + [ Issue(title="issue3", closed=(today+timedelta(3)) , labels=[] ) ]
+issues = issues + [ Issue(title="issue4", closed=(today-timedelta(7)) , labels=[] ) ]
+
 last = []
 next = []
 late = []
 
+report = []
+
 for issue in issues:
-    if issue['closed'] > thirty_last and issue['closed'] < today:
+    if issue.closed > thirty_last and issue.closed < today:
         last.append(issue)
-    if issue['closed'] > today and issue['closed'] < thirty_next:
+    if issue.closed > today and issue.closed < thirty_next:
         next.append(issue)
-    if 'Overdue' in issue['labels']:
+    if 'Overdue' in issue.labels:
         late.append(issue)
 
-print('# %(project)s Status Report for %(date)s' %
+report.append('# %(project)s Status Report for %(date)s\n' %
     { "project": 'Automated Server Provisioning', "date": today })
 
-print('\n## Tasks closed since %(date)s: %(tasks)d\n' %
+report.append('\n## Tasks closed since %(date)s: %(tasks)d\n' %
     {  "date": thirty_last, "tasks": len(last) })
 if len(last) > 0:
     for issue in last:
-        print('- %(title)s' % { "title": issue['title'] })
+        report.append('\n- %(title)s' % { "title": issue.title })
 else:
-    print('- No closed tasks')
+    report.append('\n- No closed tasks')
 
-print('\n## Tasks planned before %(date)s: %(tasks)d\n' %
+report.append('\n## Tasks planned before %(date)s: %(tasks)d\n' %
     {  "date": thirty_next, "tasks": len(next) })
 if len(next) > 0:
     for issue in next:
-        print('- %(title)s' % { "title": issue['title'] })
+        report.append('\n- %(title)s' % { "title": issue.title })
 else:
-    print('- No upcoming tasks')
+    report.append('\n- No upcoming tasks')
 
-print('\n## Overdue tasks: %(tasks)d\n' %
+report.append('\n## Overdue tasks: %(tasks)d\n' %
     { "tasks": len(late), "date": thirty_last })
 if len(late) > 0:
     for issue in late:
-        print('- %(title)s' % { "title": issue['title'] })
+        report.append('\n- %(title)s' % { "title": issue.title })
 else:
-    print('- No overdue tasks')
+    report.append('\n- No overdue tasks')
+
+report_name = 'status-' + today.strftime('%Y-%m-%d') + '.md'
+report_file = open(report_name,'w')
+report_file.writelines(report)
+report_file.close()
