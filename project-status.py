a058f0442741652c02591c76e155cb89b9455045
Walter Rowe <walter.rowe@gmail.com>
2022-05-21 13:52:55 -0400

    updated to use class module

--- project-status.py
+++ project-status.py
@@ -1,32 +1,9 @@
+#! /usr/local/bin/python3
 # Online Python - IDE, Editor, Compiler, Interpreter
 # https://www.online-python.com
 
 from datetime import *
 import issue
-
-# # Issue class used for testing
-# class Issue():
-#     def __init__(self, title, labels, closed):
-#         self.title = title
-#         self.labels = labels
-#         self.closed = closed
-#
-#     def __repr__(self):
-#         return str(self.__class__) + ': ' + str(self.__dict__)
-#
-#     def __str__(self):
-#         return f'Title: {self.title}, Closed: {self.closed}, Labels: {self.labels}'
-#
-#     def __add__(x, y):
-#         return [ x, y ]
-#
-#     def print(self):
-#         if isinstance(self,list):
-#             for mine in self:
-#                 mine.print
-#         else:
-#             print (f'Title: {self.title}, Closed: {self.closed}, Labels: {self.labels}')
-
 
 # relative dates for report
 today = date.today()
