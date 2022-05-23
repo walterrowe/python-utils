d73d808f61037b33116b09f0bca18a1308d7bf54
Walter Rowe <walter.rowe@gmail.com>
2022-05-23 19:27:43 -0400

    implement docstring and --help for python script

--- test_argv.py
+++ test_argv.py
@@ -1,11 +1,24 @@
 #! /usr/local/bin/python3
+"""
+test_argv.py [ --help ] [ --group=group[,group...] ] [ --project=project[,project...] ] [ --commit ]
 
+parameters:
+    --help: prints this usage message
+        (default is False)
+    --group: comma separated list of one or more project paths or ids
+    --project: comma separated list of one or more group paths or ids
+    --config: section of ~/.python-config.cfg to load
+    --commit: tells the script to make the change
+        (default is False -- dry-run mode)
+
+    group and project are mutually exclusive (specify one or the other, not both)
+"""
 import sys
 import argv
 
-args = argv.Parse(sys.argv, 'group=exclusive', 'project=exclusive', 'config', 'commit')
-if args.passed:
-    print(args)
+args = argv.Parse(sys.argv, 'help', 'group=exclusive', 'project=exclusive', 'config', 'commit')
+if args.passed != True or args.get('help') == True:
+    print(__doc__)
+    exit()
 
-if args.get('commit') == True:
-    print('commit = true')
+print(args)
