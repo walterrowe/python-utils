13bfbbdba87ed9a4cb7ffac4f9098799e212d5e4
Walter Rowe <walter.rowe@gmail.com>
2022-05-25 05:00:48 -0400

    add action='extend' to allow specifying an arg multiple times

--- test_argparse.py
+++ test_argparse.py
@@ -11,8 +11,8 @@ import argparse
 
 parser = argparse.ArgumentParser(add_help=True, formatter_class=argparse.RawDescriptionHelpFormatter, epilog=__doc__)
 group = parser.add_mutually_exclusive_group()
-group.add_argument('--group', dest='group', type=str, help='list of one or more group paths or ids', nargs='+')
-group.add_argument('--project', dest='project', type=str, help='list of one or more project paths or ids', nargs='+')
+group.add_argument('--group', dest='group', type=str, help='list of one or more group paths or ids', nargs='+', action='extend')
+group.add_argument('--project', dest='project', type=str, help='list of one or more project paths or ids', nargs='+', action='extend')
 parser.add_argument('--label', dest='label', type=str, help='name of label to use for overdue items', default='Overdue')
 parser.add_argument('--color', dest='color', type=str, help='name of label to use for overdue items', default='red')
 parser.add_argument('--section', dest='section', type=str, help='section of ~/.python-gitlab.cfg to load', default='default')
